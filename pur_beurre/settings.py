071852cf53f005e00cd770916081181d1a2030ee
Heights Design <plheric@gmail.com>
2021-11-14 15:41:18 +0100

    collected static

--- pur_beurre/settings.py
+++ pur_beurre/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-h$1u@^a0ai2+^rsd@4(_y&tlri&ut)0rs8yitqkbtyk3cfz2w*'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-h$1u@^a0ai2+^rsd@4(_y&tlri&ut)0rs8yitqkbtyk3cfz2w*'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
