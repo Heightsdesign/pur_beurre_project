c50b7f2157dd703e2c9f187ae56ba4ee91bd3f7d
Heights Design <plheric@gmail.com>
2021-11-30 14:50:10 +0100

    working on settings

--- pur_beurre/settings/__init__.py
+++ pur_beurre/settings/__init__.py
@@ -12,24 +12,21 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-h$1u@^a0ai2+^rsd@4(_y&tlri&ut)0rs8yitqkbtyk3cfz2w*'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'purbeurreprojectpaul.herokuapp.com']
+# ADD heroku 'purbeurreprojectpaul.herokuapp.com'
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
@@ -149,6 +146,7 @@ AUTHENTICATION_BACKENDS = (
     )
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
-django_heroku.settings(locals())
+
+# django_heroku.settings(locals())
 
 
