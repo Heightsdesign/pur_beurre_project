807103198134be6407ba1b0e86ebd59b470330a6
Heightsdesign <plheric@gmail.com>
2021-12-03 21:10:33 +0000

    modified tests

--- users/tests.py
+++ users/tests.py
@@ -10,7 +10,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import chromedriver_binary.chromedriver_filename
+import chromedriver_binary
 
 class UserPageTestCase(TestCase):
 
